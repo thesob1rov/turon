bd08d1203f7c1fbb33b3ac4935d760dd8861da01
Mannopboy <mannopboy8@gmail.com>
2023-11-18 14:35:35 +0500

    commit 3

--- backend/worker_salary/views.py
+++ backend/worker_salary/views.py
@@ -32,3 +32,66 @@ def worker():
     return render_template('worker_salary/index.html', workers=workers, user=user, news=news, jobs=jobs,
                            about_us=about_us, about_id=about_id, pages=pages,
                            subjects=subjects)
+
+
+@app.route('/worker_profile/<int:worker_id>', methods=['POST', 'GET'])
+def worker_profile(worker_id):
+    """
+    worker profili
+    :param teacher_id: kirilgan teacherni id si
+    :return:
+    """
+    error = check_session()
+    if error:
+        return redirect(url_for('home'))
+    user = current_user()
+    # teacher = Teacher.query.filter(Teacher.id == teacher_id).first()
+    teacher_birth_year = teacher.user.birth_date
+    current_year = datetime.now()
+
+    age = int(current_year.year) - int(teacher_birth_year.year)
+    about_us = TypeInfo.query.filter(TypeInfo.id == 1).first()
+    news = TypeInfo.query.filter(TypeInfo.id == 2).first()
+    jobs = TypeInfo.query.filter(TypeInfo.id == 3).first()
+    about = Info.query.filter(Info.type_id == about_us.id).order_by(Info.id).first()
+    about_id = 0
+    if about:
+        about_id = about.id
+    if about_us:
+        about_id = about_us.id
+    return render_template('worker_profile/index.html', teacher=teacher, age=age, about_us=about_us,
+                           news=news, jobs=jobs, about_id=about_id, user=user)
+
+
+@app.route('/register_worker', methods=["POST", "GET"])
+def register_worker():
+    """
+    worker registratsiya qilish
+    :return:
+    """
+    error = check_session()
+    if error:
+        return redirect(url_for('home'))
+    subjects = Subject.query.all()
+    if request.method == "POST":
+        username = request.form.get("username")
+        name = request.form.get("name")
+        surname = request.form.get("surname")
+        parent_name = request.form.get("parent_name")
+        day = request.form.get("day")
+        month = request.form.get("month")
+        year = request.form.get("year")
+        password = request.form.get("password")
+        # work_id = request.form.get("work_id")
+        number = request.form.get("number")
+        hashed = generate_password_hash(password=password, method="sha256")
+
+        datetime_str = f'{year}-{month}-{day}'
+        datetime_object = datetime.strptime(datetime_str, '%Y-%m-%d')
+        add = User(name=name, username=username, surname=surname, parent_name=parent_name, birth_date=datetime_object,
+                   password=hashed, number=number)
+        add.add()
+        worker = Worker(user_id=add.id)
+        worker.add()
+        return redirect(url_for('register'))
+    return render_template("worker_register/index.html", subjects=subjects)
--- backend/workers/views.py
+++ backend/workers/views.py
@@ -1,37 +1,5 @@
 from app import *
 from backend.models.basic_model import *
-@app.route('/register_worker', methods=["POST", "GET"])
-def register_worker():
-    """
-    worker registratsiya qilish
-    :return:
-    """
-    error = check_session()
-    if error:
-        return redirect(url_for('home'))
-    subjects = Subject.query.all()
-    if request.method == "POST":
-        username = request.form.get("username")
-        name = request.form.get("name")
-        surname = request.form.get("surname")
-        parent_name = request.form.get("parent_name")
-        day = request.form.get("day")
-        month = request.form.get("month")
-        year = request.form.get("year")
-        password = request.form.get("password")
-        # work_id = request.form.get("work_id")
-        number = request.form.get("number")
-        hashed = generate_password_hash(password=password, method="sha256")
-
-        datetime_str = f'{year}-{month}-{day}'
-        datetime_object = datetime.strptime(datetime_str, '%Y-%m-%d')
-        add = User(name=name, username=username, surname=surname, parent_name=parent_name, birth_date=datetime_object,
-                   password=hashed, number=number)
-        add.add()
-        worker = Worker(user_id=add.id)
-        worker.add()
-        return redirect(url_for('register'))
-    return render_template("worker_register/index.html", subjects=subjects)
 
 
 @app.route('/workers', methods=['POST', 'GET'])
@@ -69,7 +37,7 @@ def workers():
     if about:
         about_id = about.id
     return render_template('workers/index.html', workers_list=workers_list, jobs=jobs, jobs_list=jobs_list,
-                           about_us=about_us, about_id=about_id, news=news,user=user)
+                           about_us=about_us, about_id=about_id, news=news, user=user)
 
 
 @app.route('/edit_worker/<int:worker_id>', methods=['POST'])
